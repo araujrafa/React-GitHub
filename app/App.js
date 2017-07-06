81b292565194ff6c09fd07b58b28b14570195f89
Rafael Araujo <rafael.araujo@grupofolha.com.br>
2017-07-06 15:47:20 -0300

    arquivos

---
+++ app/App.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var GitHub = require('./components/GitHub')
+
+ReactDom.render(
+  <GitHub />, document.getElementById('app')
+);
